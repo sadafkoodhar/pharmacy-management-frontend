3a3527082b081f0b8e687a18df5387b97d9c5630
Sadaf Koondhar <sadaf.khoondhar@xpertdigital.co>
2026-05-15 11:11:06 +0500

    Final URL update for production

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
 export const Config = {
     // Aapka Spring Boot ka base URL
-    // Agar aapka backend 8080 par chal raha hai to:
-    CORE_API: "http://localhost:8080/api"
-    
-    // Agar future mein kisi aur port ya server par jaye to sirf yahan change hoga
+    // CORE_API: "http://localhost:8080/api"
+    CORE_API: "https://pharmacy-management-backend-project-production.up.railway.app/api"
     // CORE_API: "http://localhost:8020/api" 
 };
