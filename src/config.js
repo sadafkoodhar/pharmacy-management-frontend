6e0709cf6acb99ea7e9b86a5052671b292b721d9
sadafkoodhar <161293628+sadafkoodhar@users.noreply.github.com>
2026-05-21 09:48:03 +0500

    Update CORE_API to production URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const Config = {
     // Aapka Spring Boot ka base URL
-     CORE_API: "http://localhost:8080/api"
-  //  CORE_API: "https://pharmacy-management-backend-project-production.up.railway.app/api"
+// CORE_API: "http://localhost:8080/api"
+  CORE_API: "https://pharmacy-management-backend-project-production.up.railway.app/api"
     // CORE_API: "http://localhost:8020/api" 
 };
