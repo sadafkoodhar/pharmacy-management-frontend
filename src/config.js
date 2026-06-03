67da01313d7dbdb885663dac5c3212e9d7291de9
sadafkoodhar <161293628+sadafkoodhar@users.noreply.github.com>
2026-06-03 17:15:56 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export const Config = {
     // Aapka Spring Boot ka base URL
  //CORE_API: "http://localhost:8080/api"
 // CORE_API: "https://pharmacy-management-backend-project-production.up.railway.app/api"
-CORE_API: "https://citihaelthcare-production.up.railway.app/api"
+CORE_API: "https://miraculous-inspiration-production.up.railway.app/api"
     // CORE_API: "http://localhost:8020/api" 
 };
